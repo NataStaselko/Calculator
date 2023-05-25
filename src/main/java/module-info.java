3e0461ea8984b9bb079888033d44c7cf74ab6bc0
Staselko <Nataselko2021@mail.ru>
2023-05-25 22:20:04 +0300

    add Launcher class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,6 @@
 module com.staselko.calculator {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
     opens com.staselko.calculator to javafx.fxml;
     opens com.staselko.calculator.controller to javafx.fxml ;
     exports com.staselko.calculator;
